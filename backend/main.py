8eda044fade588d189813d4e59df12a7e9c02914
mrinmay2004 <mrinmay21012204@gmail.com>
2025-08-23 23:08:01 +0530

    Updated

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,15 @@
 # from urllib import response
 from fastapi import FastAPI, Depends, status, HTTPException, Query, Response,Query
 from fastapi.middleware.cors import CORSMiddleware
+import os
 # from sqlalchemy.orm import Session
 # from models import Department ,Teacher,Student,Subject,SubjectTeacher
 # from db import SessionLocal, engine
 # import schemas,models
 
 
-from routers import department, teacher, student, subject,sub_teacher,slot,comp,authentication,attendance
-
+from routers import department, teacher, student, subject,sub_teacher,slot,comp,authentication,attendance,routine,notice
+# from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 # ==========================
@@ -16,7 +17,8 @@ app = FastAPI()
 # ==========================
 origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://attendance-manage-rdyu.vercel.app",
 ]
 
 app.add_middleware(
@@ -28,7 +30,7 @@ app.add_middleware(
     expose_headers=["Content-Range"]  # important
 )
 
-
+# app.mount("/static", StaticFiles(directory="uploads"), name="static")
 # ==========================
 app.include_router(authentication.router)
 app.include_router(comp.router)
@@ -39,7 +41,10 @@ app.include_router(subject.router)
 app.include_router(sub_teacher.router)
 app.include_router(slot.router)
 app.include_router(attendance.router)
+app.include_router(routine.router)
+app.include_router(notice.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
