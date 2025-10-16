7f8badad3c76992854b9cd12ab57ae1046d95a6b
mrinmay2004 <mrinmay21012204@gmail.com>
2025-10-16 13:43:38 +0530

    Updated

--- frontend/attnendance-app/src/components/api.jsx
+++ frontend/attnendance-app/src/components/api.jsx
@@ -1,48 +1,6 @@
 const API_BASE = import.meta.env.VITE_API_URL;
 
 
-// export async function apiFetch(url, method = "GET", body = null) {
-//   const options = {
-//     method,
-//     headers: { "Content-Type": "application/json" },
-//   };
-
-//   if (body) {
-//     options.body = JSON.stringify(body);
-//   }
-
-//   const res = await fetch(`${API_BASE}${url}`, options);
-
-//   if (!res.ok) {
-//     throw new Error(`API error: ${res.status} ${res.statusText}`);
-//   }
-
-//   return res.json();
-// }
-
-
-// export async function apiFetch(url, method = "GET", body = null, isFormData = false) {
-//   const options = { method, headers: {} };
-
-//   if (body) {
-//     if (isFormData) {
-//       options.body = body; // FormData, no Content-Type header
-//     } else {
-//       options.headers["Content-Type"] = "application/json";
-//       options.body = JSON.stringify(body);
-//     }
-//   }
-
-//   const response = await fetch(`http://localhost:8000${url}`, options);
-//   if (!response.ok) {
-//     const err = await response.json().catch(() => ({}));
-//     throw new Error(err.detail || "API error");
-//   }
-//   return response.json();
-// }
-
-
-
 export async function apiFetch(url, method = "GET", body = null, isFormData = false) {
   const options = { method, headers: {} };
 
